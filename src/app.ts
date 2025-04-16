4e6f323ce007df790e22c67ac38d3692bedffba3
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-04-16 08:40:41 -0600

    http y http2

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,43 @@
+import fs from 'fs';
+import http2 from 'http2';
 
-import http from 'http';
-
-const server = http.createServer((req, res) => {
+const server = http2.createSecureServer({
+    key: fs.readFileSync('./keys/server.key'),
+    cert: fs.readFileSync('./keys/server.crt'),
+}, (req, res) => {
     console.log(req.url);
-    res.write('hola mundo');
-    res.end();
+
+    // res.writeHead(200, {'Content-Type': 'text-html'});
+    // res.write(`<h1>url ${req.url}</h1>`);
+    // res.end();
+
+    // const data = {name: 'marcelo', age: 30, city: 'new york'};
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.end(JSON.stringify(data));
+
+    if (req.url === '/') {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text-html' });
+        res.end(htmlFile);
+
+        return;
+    }
+
+    if (req.url?.endsWith('.js')) {
+        res.writeHead(200, { 'Content-Type': 'application/javascript' });
+    } else if (req.url?.endsWith('.css')) {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+    }
+
+    try {
+        const responseContent = fs.readFileSync(`./public${req.url}`, 'utf-8');
+        res.end(responseContent);
+    } catch (error) {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end();
+    }
+
+
 });
 
 server.listen(8080, () => {
