deeab9882af453591147a5f738be015ea017beba
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-04-16 10:29:30 -0600

    Operación leer del servidor rest

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
 
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -12,11 +13,13 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start() {
@@ -26,12 +29,16 @@ export class Server {
         //* public folder
         this.app.use(express.static(this.publicPath));
 
+        //* routes
+        this.app.use(this.routes);
+
+        // esto funciona en la versión 4.8
         // this.app.get('*', (req, res) => {
         //     console.log(req.url);
         //     res.send('hola mundo');
         // });
 
-        // Manejador de rutas para SPA    
+        //* Manejador de rutas para SPA    
         this.app.use((req, res) => {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             // res.sendFile(path.join(process.cwd(), "public", "index.html")); 
---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express"
+
+const todos = [
+    {id: 1, text: 'buy milk', createdAt: new Date()},
+    {id: 2, text: 'buy bread', createdAt: null},
+    {id: 3, text: 'buy butter', createdAt: new Date()},
+];
+
+export class TodosController {
+
+    //* DI
+    constructor() {
+
+    }
+
+    public getTodos = (req: Request, res: Response) => {
+        res.json(todos);
+
+        return;
+    }
+
+    public getTodoById = (req: Request, res: Response) => {
+        const id = +req.params.id;
+        if(isNaN(id)) res.status(400).json({error: `ID argument is not a number`});
+
+        const todo = todos.find(todo => todo.id === id);
+        
+        (todo) ? res.json(todo) : res.status(400).json({
+            error: `Todo with id ${id} not found`
+        })
+
+        
+    }
+}
